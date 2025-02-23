2d3d68f392b32f903853aa6bc9ec727885abe676
RehanGith <chrehan1007@gmail.com>
2025-02-23 15:23:33 +0500

    created desired variables in RegisterFragments

--- app/src/main/java/com/example/my_wallpapers/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/my_wallpapers/fragments/RegisterFragment.kt
@@ -1,9 +1,21 @@
 package com.example.my_wallpapers.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.my_wallpapers.R
+import com.example.my_wallpapers.databinding.FragmentRegisterBinding
+import com.example.my_wallpapers.viewmodels.RegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RegisterFragment: Fragment(R.layout.fragment_register) {
+    private lateinit var binding: FragmentRegisterBinding
+    private val registerViewModel by viewModels<RegisterViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRegisterBinding.bind(view)
+    }
 }
