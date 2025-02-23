e1f865ceb6594659a3927951854634e729c065d2
RehanGith <chrehan1007@gmail.com>
2025-02-23 15:19:10 +0500

    Added entrypoint annotation in Fragment and MainActivity

--- app/src/main/java/com/example/my_wallpapers/app/MainActivity.kt
+++ app/src/main/java/com/example/my_wallpapers/app/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.my_wallpapers.R
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/my_wallpapers/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/my_wallpapers/fragments/RegisterFragment.kt
@@ -2,6 +2,8 @@ package com.example.my_wallpapers.fragments
 
 import androidx.fragment.app.Fragment
 import com.example.my_wallpapers.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RegisterFragment: Fragment(R.layout.fragment_register) {
 }
