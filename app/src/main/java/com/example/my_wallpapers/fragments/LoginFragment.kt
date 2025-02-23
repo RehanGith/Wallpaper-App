2c18b42f27e69e25ab91a9a48c920fcbf9d310c7
RehanGith <chrehan1007@gmail.com>
2025-02-23 18:10:35 +0500

    Sucessfully added the logic to reset the Email

--- app/src/main/java/com/example/my_wallpapers/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/my_wallpapers/fragments/LoginFragment.kt
@@ -3,6 +3,7 @@ package com.example.my_wallpapers.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -10,6 +11,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.example.my_wallpapers.R
 import com.example.my_wallpapers.databinding.FragmentLoginBinding
+import com.example.my_wallpapers.dialog.setUpBottomDialog
 import com.example.my_wallpapers.util.Response
 import com.example.my_wallpapers.viewmodels.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,8 +27,9 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentLoginBinding.bind(view)
         binding.tvForgotPasswordLogin.setOnClickListener {
-            val email = binding.edEmailLogin.text.toString().trim()
-            loginViewModel.resetPassword(email)
+            setUpBottomDialog {
+                loginViewModel.resetPassword(it)
+            }
         }
         binding.buttonLoginLogin.setOnClickListener {
             val email = binding.edEmailLogin.text.toString().trim()
@@ -59,13 +62,20 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 loginViewModel.resetPass.collect {
                     when(it) {
-                        is Response.Failure -> TODO()
-                        is Response.Loading -> TODO()
-                        is Response.Success -> TODO()
-                        is Response.UnSpecifies -> TODO()
+                        is Response.Failure -> {
+                            Toast.makeText(requireContext(), "Something is wrong with your Email", Toast.LENGTH_LONG).show()
+                        }
+                        is Response.Loading -> {
+                            Log.d("LoginFragment", "onViewCreated: Loading")
+                        }
+                        is Response.Success -> {
+                            Toast.makeText(requireContext(), "Email is ben Sent to you email", Toast.LENGTH_LONG).show()
+                        }
+                        is Response.UnSpecifies -> Unit
                     }
                 }
             }
         }
+
     }
 }
