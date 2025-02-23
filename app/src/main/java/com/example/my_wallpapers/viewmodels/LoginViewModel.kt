51c91ee9159ceb23f7a93c619b748cb5fb0b1f27
RehanGith <chrehan1007@gmail.com>
2025-02-23 16:30:35 +0500

    created the login logic in LoginViewModel

--- app/src/main/java/com/example/my_wallpapers/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/example/my_wallpapers/viewmodels/LoginViewModel.kt
@@ -1,8 +1,45 @@
 package com.example.my_wallpapers.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.my_wallpapers.util.Response
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val firebaseAuth: FirebaseAuth
+) : ViewModel() {
+    private var _login = MutableSharedFlow<Response<FirebaseUser>>()
+    val login = _login.asSharedFlow()
 
-class LoginViewModel : ViewModel() {
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            _login.emit(Response.Loading())
+        }
+        if(email.isEmpty() || password.isEmpty()) {
+            viewModelScope.launch {
+                _login.emit(Response.Failure("Email or password is empty"))
+            }
+        }
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                viewModelScope.launch {
+                    it.user?.apply {
+                        _login.emit(Response.Success(this))
+                    }
+                }
+            }.addOnFailureListener {
+                viewModelScope.launch {
+                    _login.emit(Response.Failure(it.message.toString()))
+                }
+            }
+    }
+
 }
