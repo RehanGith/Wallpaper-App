59f063447bcfb60233e45f3c4b55c7e02bab1257
RehanGith <chrehan1007@gmail.com>
2025-02-23 16:36:18 +0500

    observed the login in LoginFragments

--- app/src/main/java/com/example/my_wallpapers/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/my_wallpapers/fragments/LoginFragment.kt
@@ -1,9 +1,49 @@
 package com.example.my_wallpapers.fragments
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.my_wallpapers.R
+import com.example.my_wallpapers.databinding.FragmentLoginBinding
+import com.example.my_wallpapers.util.Response
+import com.example.my_wallpapers.viewmodels.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LoginFragment : Fragment(R.layout.fragment_login) {
+    private lateinit var binding: FragmentLoginBinding
+    private val loginViewModel by viewModels<LoginViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentLoginBinding.bind(view)
+
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                loginViewModel.login.collect {
+                    when(it) {
+                        is Response.Failure -> {
+                            Log.d("LoginFragment", "onViewCreated: ${it.message}")
+                            binding.buttonLoginLogin.revertAnimation()
+                        }
+                        is Response.Loading -> {
+                            binding.buttonLoginLogin.startAnimation()
+                        }
+                        is Response.Success -> {
+                            binding.buttonLoginLogin.revertAnimation()
+                        }
+                        is Response.UnSpecifies -> Unit
+                    }
+                }
+            }
+        }
+    }
 }
