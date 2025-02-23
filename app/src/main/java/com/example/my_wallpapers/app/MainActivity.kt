7c25b12695c9fbf0fa735cf58193c00c4b8ab8e6
RehanGith <chrehan1007@gmail.com>
2025-02-23 13:12:09 +0500

    Moved the mainactivity in package app

--- app/src/main/java/com/example/my_wallpapers/app/MainActivity.kt
+++ app/src/main/java/com/example/my_wallpapers/app/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.my_wallpapers
+package com.example.my_wallpapers.app
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.my_wallpapers.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
