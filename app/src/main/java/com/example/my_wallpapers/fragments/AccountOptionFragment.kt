5cfc713aace32f79d5213bcc42e1ba111e02c91b
RehanGith <chrehan1007@gmail.com>
2025-02-23 18:16:07 +0500

    Added some correction in introduction and accountOptionFragment

--- app/src/main/java/com/example/my_wallpapers/fragments/AccountOptionFragment.kt
+++ app/src/main/java/com/example/my_wallpapers/fragments/AccountOptionFragment.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.my_shoppings.R
-import com.example.my_shoppings.databinding.FragmentAccountOptionBinding
+import com.example.my_wallpapers.R
+import com.example.my_wallpapers.databinding.FragmentAccountOptionBinding
 
 class AccountOptionFragment : Fragment(R.layout.fragment_account_option) {
     private lateinit var binding: FragmentAccountOptionBinding
@@ -14,10 +14,10 @@ class AccountOptionFragment : Fragment(R.layout.fragment_account_option) {
         binding = FragmentAccountOptionBinding.bind(view)
 
         binding.btnLogin.setOnClickListener {
-            findNavController().navigate(R.id.action_accountOptionFragment2_to_loginFragment)
+
         }
         binding.btnRegister.setOnClickListener {
-            findNavController().navigate(R.id.action_accountOptionFragment2_to_registerFragment)
+
         }
     }
 }
--- app/src/main/java/com/example/my_wallpapers/fragments/IntroductionFragment.kt
+++ app/src/main/java/com/example/my_wallpapers/fragments/IntroductionFragment.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.my_shoppings.R
-import com.example.my_shoppings.databinding.FragmentIntroductionBinding
+
+import com.example.my_wallpapers.R
+import com.example.my_wallpapers.databinding.FragmentIntroductionBinding
 
 class IntroductionFragment: Fragment(R.layout.fragment_introduction) {
     private lateinit var binding: FragmentIntroductionBinding
@@ -14,7 +15,7 @@ class IntroductionFragment: Fragment(R.layout.fragment_introduction) {
         binding = FragmentIntroductionBinding.bind(view)
 
         binding.btnStart.setOnClickListener {
-            findNavController().navigate(R.id.action_introductionFragment_to_accountOptionFragment2)
+
         }
     }
 }
