f989388309d1b303c74ec90394877b92a6816709
RehanGith <chrehan1007@gmail.com>
2025-02-23 15:48:26 +0500

    created login fragment and make it as navHostFragment

---
+++ app/src/main/java/com/example/my_wallpapers/fragments/LoginFragment.kt
@@ -0,0 +1,9 @@
+package com.example.my_wallpapers.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.my_wallpapers.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment(R.layout.fragment_login) {
+}
