5a1d145c4f8828d09f34f9c94d32a35939d48b08
RehanGith <chrehan1007@gmail.com>
2025-02-23 14:19:30 +0500

    Created the firebaseAuth instance Provide function in appModle

---
+++ app/src/main/java/com/example/my_wallpapers/viewmodels/RegisterViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.my_wallpapers.viewmodels
+
+import com.google.firebase.auth.FirebaseAuth
+import javax.inject.Inject
+
+class RegisterViewModel @Inject constructor(
+    private val firebaseAuth: FirebaseAuth,
+)  {
+
+}
