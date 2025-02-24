11334414760b33f6b9311bf7e22381e6ac8bcb32
RehanGith <chrehan1007@gmail.com>
2025-02-24 18:45:42 +0500

    refector to main fragment

---
+++ app/src/main/java/com/example/my_wallpapers/fragments/wallpaperfragments/MainFragment.kt
@@ -0,0 +1,8 @@
+package com.example.my_wallpapers.fragments.wallpaperfragments
+
+import androidx.fragment.app.Fragment
+import com.example.my_wallpapers.R
+
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+}
