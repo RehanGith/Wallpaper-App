e73b1b2dd542e977cb9c9b85d9422bcc26d294e2
RehanGith <chrehan1007@gmail.com>
2025-02-24 20:46:56 +0500

    sucessfully launched the app

--- app/src/main/java/com/example/my_wallpapers/fragments/wallpaperfragments/MainFragment.kt
+++ app/src/main/java/com/example/my_wallpapers/fragments/wallpaperfragments/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.my_wallpapers.fragments.wallpaperfragments
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.my_wallpapers.R
 import com.example.my_wallpapers.adapter.ViewPagerAdapter
@@ -14,12 +15,13 @@ import kotlinx.coroutines.flow.combine
 class MainFragment : Fragment(R.layout.fragment_main) {
     private lateinit var binding: FragmentMainBinding
 
-    val tabTitles = listOf("Home", "Popular","Random", "Categories")
+    val tabTitles = listOf("Home", "Popular","Random", "Category")
     val fragments = listOf(HomeFragment(), PopularFragment(), RandomFragment(), CategoryFragment())
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMainBinding.bind(view)
+        initViewPager()
         initTabLayout()
     }
 
@@ -34,7 +36,8 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         binding.viewPager.isUserInputEnabled = false
     }
     private fun initToolBar() {
-
+        binding.customeToolbar.title = "Wallpaper"
+        (activity as AppCompatActivity).setSupportActionBar(binding.customeToolbar)
     }
 
 }
