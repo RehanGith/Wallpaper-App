32944a27449ddaed6737281c00428eec2d25627a
RehanGith <chrehan1007@gmail.com>
2025-02-24 20:12:12 +0500

    Created the viewPagerAdapter

--- app/src/main/java/com/example/my_wallpapers/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/my_wallpapers/adapter/ViewPagerAdapter.kt
@@ -1,4 +1,17 @@
 package com.example.my_wallpapers.adapter
 
-class ViewPagerAdapter () {
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.my_wallpapers.app.WallpaperActivity
+import javax.inject.Inject
+
+class ViewPagerAdapter(container : WallpaperActivity , private val fragmentList : List<Fragment>) : FragmentStateAdapter(container) {
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
 }
